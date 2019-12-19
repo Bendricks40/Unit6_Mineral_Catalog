e737a24692364467c3a6988e1504d615602d1daa
Bendricks40 <bendricks40@gmail.com>
2019-12-19 11:07:16 -0500

    finished templtemplates, started adding test cases

--- minerals/tests.py
+++ minerals/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.utils import timezone
 # Create your tests here.
+
+from .models import  Mineral
+
+class MineralModelTests(TestCase):
+    def test_mineral_creation(self):
+        mineral = Mineral.objects.create(
+            name="Ben's Test Mineral"
+        )
+        self.assertIn(mineral, Mineral.objects.all())
+
