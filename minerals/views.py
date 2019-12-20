cab494d29cf3e6661de2f6ce48a6eb653b30f24d
Bendricks40 <bendricks40@gmail.com>
2019-12-19 21:42:59 -0500

    built out the random mineral generator button

--- minerals/views.py
+++ minerals/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Mineral
+import random
 
 # Create your views here.
 def mineral_list(request):
     minerals = Mineral.objects.all()
-    return render(request, 'minerals/mineral_list.html', {'minerals': minerals})
+    count = Mineral.objects.count()
+    randMineral = random.randrange(1, count)
+    return render(request, 'minerals/mineral_list.html', {'minerals': minerals, 'randMineral': randMineral})
 
 def mineral_detail(request, pk):
     mineral = Mineral.objects.get(pk=pk)
-    return render(request, 'minerals/mineral_detail.html', {'mineral': mineral})
+    count = Mineral.objects.count()
+    randMineral = random.randrange(1, count)
+    return render(request, 'minerals/mineral_detail.html', {'mineral': mineral, 'randMineral': randMineral})
